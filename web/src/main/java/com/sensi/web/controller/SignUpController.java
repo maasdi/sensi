6c201ad4cf8d1b139820774ead71feb116d537ea
Maas Dianto <maas.dianto@gmail.com>
2012-07-17 18:03:13 +0700

    SignUpController change

--- web/src/main/java/com/sensi/web/controller/SignUpController.java
+++ web/src/main/java/com/sensi/web/controller/SignUpController.java
@@ -1,14 +1,11 @@
 package com.sensi.web.controller;
 
-import javax.validation.Valid;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
