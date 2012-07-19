4ecace11c9b4056a0696473a5b288346643444c1
maasdianto <maasdianto@maas-Inspiron>
2012-07-19 23:59:36 +0700

    create constant util

---
+++ web/src/main/java/com/sensi/web/util/Constants.java
@@ -0,0 +1,15 @@
+package com.sensi.web.util;
+
+/**
+*
+* @author <a href="mailto:maas.dianto@gmail.com">Maas Dianto</a>
+* Jul 19, 2012
+*/
+public class Constants {
+	
+	public static final String ROLE_USER = "ROLE_USER";
+	
+	private Constants() {
+	}
+
+}
