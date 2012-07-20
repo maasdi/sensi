6bcf009d73b599ba43e8af513b5a23425248e6c5
maasdianto <maas.dianto@gmail.com>
2012-07-21 05:03:38 +0700

    create forgot password domain

---
+++ domain/src/main/java/com/sensi/domain/ForgotPassword.java
@@ -0,0 +1,22 @@
+package com.sensi.domain;
+
+/**
+ * domain class for ForgotPassword form
+ *
+ * @author <a href="mailto:maas.dianto@gmail.com">Maas Dianto</a>
+ * Jul 21, 2012
+ */
+
+public class ForgotPassword {
+	
+	private String email;
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+}
